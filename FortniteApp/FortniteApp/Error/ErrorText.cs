f28d7d826f69879b62486b7b5129d83193c23879
Jesper Engdahl <p3pp33ng@gmail.com>
2018-02-05 10:58:09 +0100

    Set up a error object to handle errors

---
+++ FortniteApp/FortniteApp/Error/ErrorText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FortniteApp.Error
+{
+    public static class ErrorText
+    {
+        public static string BadConnection()
+        {
+            return "Bad connection, please try later";
+        }
+
+        public static string WereNotFound()
+        {
+            return "The list was not found";
+        }
+    }
+}
