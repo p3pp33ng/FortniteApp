2a13b1390c8064e3ff067c1cdad93ab30d855709
Jesper Engdahl <p3pp33ng@gmail.com>
2018-02-05 10:58:40 +0100

    Set up Fetching the data from the API

---
+++ FortniteApp/FortniteApp/Fetch/Fetcher.cs
@@ -0,0 +1,61 @@
+﻿using FortniteApp.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortniteApp.Fetch
+{
+    public class Fetcher
+    {
+        public async Task<WeaponsList> FetchWeapon()
+        {
+            var result = new WeaponsList();
+            using (var client = SetUpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync($"http://www.fortnitechests.info/api/weapons");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = JsonConvert.DeserializeObject<WeaponsList>(await response.Content.ReadAsStringAsync());
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return result;
+        }
+
+        public async Task<ChestDTO> FetchChests()
+        {
+            var result = new ChestDTO();
+            using (var client = SetUpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync($"http://www.fortnitechests.info/api/chests");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = JsonConvert.DeserializeObject<ChestDTO>(await response.Content.ReadAsStringAsync());
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return result;
+        }
+
+        private HttpClient SetUpClient()
+        {
+            HttpClient client = new HttpClient();
+            return client;
+        }
+    }
+}
